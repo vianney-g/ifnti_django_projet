2865a685a9e8991596f236bbe538fb48b7ce37ae
Vianney Gremmel <loutre.a@gmail.com>
2026-09-16 16:36:29 +0000

    Add ruff and format all

--- stages/migrations/0001_initial.py
+++ stages/migrations/0001_initial.py
@@ -4,26 +4,32 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Entreprise',
+            name="Entreprise",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=120, unique=True)),
-                ('ville', models.CharField(max_length=80)),
-                ('secteur', models.CharField(max_length=80)),
-                ('contact', models.EmailField(max_length=254)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=120, unique=True)),
+                ("ville", models.CharField(max_length=80)),
+                ("secteur", models.CharField(max_length=80)),
+                ("contact", models.EmailField(max_length=254)),
             ],
             options={
-                'verbose_name': 'entreprise',
-                'verbose_name_plural': 'entreprises',
-                'ordering': ['nom'],
+                "verbose_name": "entreprise",
+                "verbose_name_plural": "entreprises",
+                "ordering": ["nom"],
             },
         ),
     ]
